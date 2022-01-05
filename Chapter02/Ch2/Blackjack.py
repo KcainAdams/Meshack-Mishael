b611593b5c1a2af274b5f27b076e6a6d8e4bab0d
Frankline Sable <franklineodero@gmail.com>
2022-01-05 15:26:40 +0300

    fixing bug in the document reference

--- Chapter02/Ch2/Blackjack.py
+++ Chapter02/Ch2/Blackjack.py
@@ -2,18 +2,29 @@ import os
 import tkinter as ttk
 import random
 
-assets_folder = os.path.abspath(os.path.join(os.path.dirname(__file__),
-'..', 'assets/'))
+assets_folder = os.path.join(os.path.dirname(__file__),'assets\\')
+
+# stop using absolute paths,
+# --->os.path.abspath(os.path.join(os.path.dirname(__file__),'..', 'assets/'))
+# outputs---> C:\Users\Frankline Sable\PycharmProjects\Meshack-Mishael\Chapter02\assets
+
+# using relative paths to locate an object
+# --->os.path.join(os.path.dirname(__file__),'assets\\')
+# outputs---> C:\Users\Frankline Sable\PycharmProjects\Meshack-Mishael\Chapter02\Ch2\assets\
+
 class Card:
+    print(assets_folder)
+
     def __init__(self, suit, value):
         self.suit = suit
         self.value = value
 
     def __repr__(self):
         return " of ".join((self.value, self.suit))
+
     @classmethod
     def get_back_files(cls):
-        cls.back = ttk.PhotoImage(file= assets_folder + "/back.png")
+        cls.back = ttk.PhotoImage(file=assets_folder + "/back.png")
 
         return cls.back
 
@@ -29,7 +40,7 @@ class Deck:
 
     def deal(self):
         if len(self.cards) > 1:
-            return self.cards.pop(0)#returns  the top card and removes it from thr deck so that its not used again
+            return self.cards.pop(0)  # returns  the top card and removes it from thr deck so that its not used again
 
 
 class Hand:
@@ -61,6 +72,7 @@ class Hand:
         self.calculate_value()
         return self.value
 
+
 class Gamestate:
     def __init__(self):
         self.deck = Deck()
@@ -70,13 +82,13 @@ class Gamestate:
         self.dealer_hand = Hand(dealer=(True))
 
         for i in range(2):
-             self.player_hand.add_card(self.deck.deal())
-             self.dealer_hand.add_card(self.deck.deal())
+            self.player_hand.add_card(self.deck.deal())
+            self.dealer_hand.add_card(self.deck.deal())
 
         self.has_winner = ''
 
     def player_is_over(self):
-         return self.player_hand.get_value() > 21
+        return self.player_hand.get_value() > 21
 
     def someone_has_blackjack(self):
         player = False
@@ -84,7 +96,7 @@ class Gamestate:
         if self.player_hand.get_value() == 21:
             player = True
         if self.dealer_hand.get_value() == 21:
-           dealer = True
+            dealer = True
         if player and dealer:
             return 'dp'
         elif player:
@@ -95,13 +107,13 @@ class Gamestate:
         return False
 
     def hit(self):
-      self.player_hand.add_card(self.deck.deal())
-      if self.someone_has_blackjack() == 'p':
-          self.has_winner = 'p'
-      if self.player_is_over():
-          self.has_winner = 'd'
+        self.player_hand.add_card(self.deck.deal())
+        if self.someone_has_blackjack() == 'p':
+            self.has_winner = 'p'
+        if self.player_is_over():
+            self.has_winner = 'd'
 
-          return self.has_winner
+            return self.has_winner
 
     def get_table_state(self):
         blackjack = False
@@ -115,7 +127,7 @@ class Gamestate:
                     'dealer_cards': self.dealer_hand.cards,
                     'has_winner': winner,
                     'blackjack': blackjack,
-                    }
+                }
                 return table_state
 
     def calculate_final_state(self):
@@ -128,41 +140,43 @@ class Gamestate:
         else:
             winner = 'd'
         table_state = {
-                'player_cards': self.player_hand.cards,
-                'dealer_cards': self.dealer_hand.cards,
-                'has_winner': winner,
-                }
+            'player_cards': self.player_hand.cards,
+            'dealer_cards': self.dealer_hand.cards,
+            'has_winner': winner,
+        }
         return table_state
 
     def player_score_as_text(self):
         return "Score: " + str(self.player_hand.get_value())
+
 
 class GameScreen(ttk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Blackjack")
         self.geometry('800x640')
-        self.resizable(False,False)
+        self.resizable(False, False)
         self.CARD_ORIGINAL_POSITION = 100
         self.CARD_WIDTH_OFFSET = 100
-        self.PLAYER_CARD_HEIGHT =300
-        self.DEALER_CARD_HEIGHT =100
-        self.PLAYER_SCORE_TEXT_COORDS = (400,450)
-        self.WINNER_TEXT_COORDS = (400,450)
+        self.PLAYER_CARD_HEIGHT = 300
+        self.DEALER_CARD_HEIGHT = 100
+        self.PLAYER_SCORE_TEXT_COORDS = (400, 450)
+        self.WINNER_TEXT_COORDS = (400, 450)
 
         self.game_state = Gamestate()
-        self.game_screen = ttk.Canvas(self,bg = 'white',width = 800,height = 500)
-        self.bottom_frame = ttk.Frame(self,width = 800,height = 140,bg = 'red')
+        self.game_screen = ttk.Canvas(self, bg='white', width=800, height=500)
+        self.bottom_frame = ttk.Frame(self, width=800, height=140, bg='red')
         self.bottom_frame.pack_propagate(0)
-        self.hit_button = ttk.Button(self.bottom_frame,text = 'Hit',width=25,command=self.hit)
-        self.stick_button = ttk.Button(self.bottom_frame,text = 'Stick',width =25,command = self.stick)
-        self.play_again_button = ttk.Button(self.bottom_frame,text = 'Play Again',command = self.play_again)
-        self.quit_button = ttk.Button(self.bottom_frame,text = 'Quit',width = 25,command = self.destroy)
-        self.hit_button.pack(side=ttk.LEFT,padx =(100,200))
+        self.hit_button = ttk.Button(self.bottom_frame, text='Hit', width=25, command=self.hit)
+        self.stick_button = ttk.Button(self.bottom_frame, text='Stick', width=25, command=self.stick)
+        self.play_again_button = ttk.Button(self.bottom_frame, text='Play Again', command=self.play_again)
+        self.quit_button = ttk.Button(self.bottom_frame, text='Quit', width=25, command=self.destroy)
+        self.hit_button.pack(side=ttk.LEFT, padx=(100, 200))
         self.stick_button.pack(side=ttk.LEFT)
-        self.bottom_frame.pack(side = ttk.BOTTOM,fill = ttk.X)
-        self.game_screen.pack(side = ttk.LEFT,anchor = ttk.N)
+        self.bottom_frame.pack(side=ttk.BOTTOM, fill=ttk.X)
+        self.game_screen.pack(side=ttk.LEFT, anchor=ttk.N)
         self.display_table()
+
     def display_table(self, hide_dealer=True, table_state=None):
         if not table_state:
             table_state = self.game_state.get_table_state()
@@ -187,9 +201,10 @@ class GameScreen(ttk.Tk):
             self.game_screen.create_image(
                 (self.CARD_ORIGINAL_POSITION + self.CARD_WIDTH_OFFSET * card_number, self.DEALER_CARD_HEIGHT),
                 image=card_image
-           )
+            )
 
-        self.game_screen.create_text(self.PLAYER_SCORE_TEXT_COORDS, text=self.game_state.player_score_as_text(), font=(None, 20))
+        self.game_screen.create_text(self.PLAYER_SCORE_TEXT_COORDS, text=self.game_state.player_score_as_text(),
+                                     font=(None, 20))
 
         if table_state['has_winner']:
             if table_state['has_winner'] == 'p':
@@ -200,29 +215,33 @@ class GameScreen(ttk.Tk):
                 self.game_screen.create_text(self.WINNER_TEXT_COORDS, text="DEALER WINS!", font=(None, 50))
 
             self.show_play_again_options()
+
     def show_play_again_option(self):
         self.hit_buttton.pack_forget()
         self.stick_button.pack_forget()
 
-        self.play_again_button.pack(side=ttk.LEFT,padx=(100,200))
+        self.play_again_button.pack(side=ttk.LEFT, padx=(100, 200))
         self.quit_button.pack(side=ttk.LEFT)
+
     def hit(self):
         self.game_state.hit()
         self.display_table()
+
     def stick(self):
-        table_state=self.game_state.calculate_final_state()
-        self.display_table(False,table_state)
+        table_state = self.game_state.calculate_final_state()
+        self.display_table(False, table_state)
+
     def play_again(self):
         self.show_gameplay_buttons
         self.game_state = Gamestate()
         self.display_table()
+
     def show_gameplay_buttons(self):
         self.play_again_button.pack_forget()
         self.quit_button.pack_forget()
 
-        self.hit_buttton.pack(side=ttk.LEFT,padx = (100,200))
+        self.hit_buttton.pack(side=ttk.LEFT, padx=(100, 200))
         self.quit_button.pack(side=ttk.LEFT)
-
 
 
 if __name__ == "__main__":
